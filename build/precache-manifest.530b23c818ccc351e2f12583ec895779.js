dc1be7d0442f495abe0be0837a6754abf615a7b9
Rafael Peroco <rafaelperoco@gmail.com>
2021-07-02 05:40:46 +0000

    first testing

---
+++ build/precache-manifest.530b23c818ccc351e2f12583ec895779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cc982d9b79692fefb618145bc322c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e75e6b689a37916f50",
+    "url": "/static/css/2.2bc212ea.chunk.css"
+  },
+  {
+    "revision": "a825297e91b3a6f1aeb2",
+    "url": "/static/css/main.1252bdff.chunk.css"
+  },
+  {
+    "revision": "14e75e6b689a37916f50",
+    "url": "/static/js/2.58fd660d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.58fd660d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a825297e91b3a6f1aeb2",
+    "url": "/static/js/main.44119201.chunk.js"
+  },
+  {
+    "revision": "af99edab7c3a110db6a6",
+    "url": "/static/js/runtime-main.4bbf50f9.js"
+  }
+]);
---
+++ build/precache-manifest.ebd20dcb475fb3e9b4fff29f86f8528e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebca4f21a0fef9c3c761cb7b4b6c25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e75e6b689a37916f50",
+    "url": "/static/css/2.2bc212ea.chunk.css"
+  },
+  {
+    "revision": "a825297e91b3a6f1aeb2",
+    "url": "/static/css/main.1252bdff.chunk.css"
+  },
+  {
+    "revision": "14e75e6b689a37916f50",
+    "url": "/static/js/2.58fd660d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.58fd660d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a825297e91b3a6f1aeb2",
+    "url": "/static/js/main.44119201.chunk.js"
+  },
+  {
+    "revision": "af99edab7c3a110db6a6",
+    "url": "/static/js/runtime-main.4bbf50f9.js"
+  }
+]);
